dab4404edf534cf4b2d52fb512bdbda8e1615772
Jon Smith <aardvarkjon94@yahoo.com>
2021-04-05 19:41:10 -0500

    Added a product detail template and links on the index page

--- index.js
+++ index.js
@@ -28,6 +28,13 @@ app.get('/products', async (req, res) => {
     res.render('products/index', { products })
 })
 
+app.get('/products/:id', async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    console.log(product)
+    res.render('products/show', { product })
+})
+
 
 //Listen for the server starting
 app.listen(port, () => {
