bf023a29ec0d758b74b169aa606e226ee4d987e2
Jon Smith <aardvarkjon94@yahoo.com>
2021-04-05 19:29:42 -0500

    Added basic products index page

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
+//Express Setup
 const express = require('express');
 const app = express();
 const path = require('path');
 const port = 3000;
+
+//Mongo Database Setup
 const mongoose = require('mongoose');
-
 const Product = require('./models/product');
-
 mongoose.connect('mongodb://localhost:27017/farmStand', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log("Mongo connection open")
@@ -14,13 +15,21 @@ mongoose.connect('mongodb://localhost:27017/farmStand', { useNewUrlParser: true,
         console.log(`Oh no, Mongo connection error: ${err}`)
     })
 
+//Setup views path and ejs view engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-    res.send('Hello World')
+
+//Routing
+
+app.get('/products', async (req, res) => {
+    const products = await Product.find({})
+    console.log(products)
+    res.render('products/index', { products })
 })
 
+
+//Listen for the server starting
 app.listen(port, () => {
     console.log(`App Listening on Port ${port}`);
 })
