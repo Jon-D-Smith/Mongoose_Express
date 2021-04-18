ad572566d73737a60e725b4fe70caa373b7e0171
Jon Smith <aardvarkjon94@yahoo.com>
2021-04-18 16:59:07 -0500

    Setup basic Farm model

---
+++ models/farm.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+
+const farmSchema = new Schema({
+    name: {
+        type: String,
+        required: [true, 'Farm must have a name!']
+    },
+    city: {
+        type: String
+    },
+    email: {
+        type: String,
+        required: [true, "Email required"]
+    }
+})
