9d734e66108b9f78d049776cbd7f96260d8bd31d
Jon Smith <aardvarkjon94@yahoo.com>
2021-04-18 22:29:33 -0500

    Added routes for linking farm and product

--- index.js
+++ index.js
@@ -53,13 +53,14 @@ app.post('/farms', async (req, res) => {
 
 app.get('/farms/:id', async (req, res) => {
     const { id } = req.params;
-    const farm = await Farm.findById(id);
+    const farm = await Farm.findById(id).populate('products');
     res.render('farms/show', { farm })
 })
 
-app.get('/farms/:id/products/new', (req, res) => {
+app.get('/farms/:id/products/new', async (req, res) => {
     const { id } = req.params;
-    res.render('products/new', { categories, id })
+    const farm = await Farm.findById(id);
+    res.render('products/new', { categories, farm })
 })
 
 app.post('/farms/:id/products', async (req, res) => {
@@ -71,7 +72,7 @@ app.post('/farms/:id/products', async (req, res) => {
     product.farm = farm;
     await farm.save();
     await product.save();
-    res.send(farm)
+    res.redirect(`/farms/${id}`)
 
 })
 
@@ -132,7 +133,7 @@ app.delete('/products/:id', async (req, res) => {
 //Product detail page
 app.get('/products/:id', async (req, res) => {
     const { id } = req.params;
-    const product = await Product.findById(id);
+    const product = await Product.findById(id).populate('farm');
     console.log(product)
     res.render('products/show', { product })
 })
